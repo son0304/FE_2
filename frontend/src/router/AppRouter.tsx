8225836446fffa635e14590954a978f59cf23bbe
Son <sonlvph46459@fpt.edu.vn>
2025-03-27 16:50:03 +0700

    hooks

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -6,9 +6,9 @@ import Dashboard from '../pages/Admin/Dashboard';
 import Login from '../auth/Login';
 import HomeUser from '../pages/Admin/User/HomeUser';
 import ListUser from '../pages/Admin/User/ListUser';
-import CreateUser from '../pages/Admin/User/CreateUser';
-import UpdateUser from '../pages/Admin/User/UpdateUser';
-import DetailUser from '../pages/Admin/User/DetailUser';
+// import CreateUser from '../pages/Admin/User/CreateUser';
+// import UpdateUser from '../pages/Admin/User/UpdateUser';
+// import DetailUser from '../pages/Admin/User/DetailUser';
 import HomeClient from '../pages/Client/HomeClient'; // Import HomeClient
 import ProductCilent from '../pages/Client/ProductCilent';
 
@@ -31,9 +31,9 @@ const AppRouter = () => {
                 {/* User Management */}
                 <Route path="user" element={<HomeUser />}>
                     <Route index element={<ListUser />} />
-                    <Route path="create" element={<CreateUser />} />
+                    {/* <Route path="create" element={<CreateUser />} />
                     <Route path="detail/:id" element={<DetailUser />} />
-                    <Route path="update/:id" element={<UpdateUser />} />
+                    <Route path="update/:id" element={<UpdateUser />} /> */}
                 </Route>
             </Route>
         </Routes>
